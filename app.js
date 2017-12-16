e9a93b47e7954a42c5b75add2f8142e6d38594de
Niel Mistry <nielmistry1@gmail.com>
2017-12-16 17:09:05 -0500

    Add words.js

--- app.js
+++ app.js
@@ -7,15 +7,9 @@ const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   res.end('Hello World\n');
-  // Tone.Transport.start();
 });
 
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
 
-
-// Tone.Transport.schedule(function(time)
-// {
-
-// }, "1m");
