4c4b1be694bf1c42e0994f5ca2aef3ba98a2156e
Niel Mistry <nielmistry1@gmail.com>
2017-12-16 13:03:55 -0500

    trying stuff

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
 const http = require('http');
 
-const hostname = '127.0.0.1';
+const hostname = '0.0.0.0';
 const port = 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   res.end('Hello World\n');
+  // Tone.Transport.start();
 });
 
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
+
+
+// Tone.Transport.schedule(function(time)
+// {
+
+// }, "1m");
